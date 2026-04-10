737127d5ddfc942e8d7c7828bc18cf0fa220581f
Demi Wright <demi.wright@icloud.com>
2026-04-10 08:55:53 +0100

    Adding trangenic line into the DAG for completeness. Editing and also adding checks for colliders to discuss in the pre registration.

--- final-DAGfor-submission.R
+++ final-DAGfor-submission.R
@@ -3,31 +3,41 @@ library(dagitty)
 library(tidyverse)
 
 # The final DAG with all confounders included
+# question 4
 
-my_dag <- dagify(
+final_assignment_dag <- dagify(
   
+  cas9_tagging ~ promoter,
+  allele_inherited ~ promoter, # This is the confounder that affects both the exposure and the outcome
   allele_inherited ~ cas9_tagging,
-  allele_inherited ~ promoter,
   allele_inherited ~ generation,
+  allele_inherited ~ transgenic_line,
   
-  # Adding the relationships between the confounders
-  cas9_tagging ~~ promoter,
-  cas9_tagging ~~ generation,
-  
-  # Label exposure and outcome
   exposure = "cas9_tagging",
   outcome = "allele_inherited",
   
-  # Assigning full labels for the plot
   labels = c(
     cas9_tagging = "Cas9 Protein Tagging",
     allele_inherited = "Allele Inherited",
     promoter = "Promoter",
-    generation = "Generation"
+    generation = "Generation",
+    transgenic_line = "Transgenic Line"
   )
 )
 
-ggdag_status(secondary_dag, use_labels = "label", text = FALSE) +
+ggdag_status(final_assignment_dag, use_labels = "label", text = FALSE) +
   theme_dag() +
   theme(legend.position = "bottom") +
   labs(title = "Causal DAG for Cas9 Tagging Inheritance")
+
+# checking for colliders
+# question 6
+
+ggdag_collider(final_assignment_dag) + 
+  theme_dag() +
+  theme(legend.position = "bottom") +
+  labs(title = "Collider Check",
+       subtitle = "Variables with arrows pointing into them")
+
+
+
