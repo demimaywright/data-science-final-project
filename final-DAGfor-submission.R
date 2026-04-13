05fb222d42e38105b1db692bf9f8394c244581d0
Demi Wright <demi.wright@icloud.com>
2026-04-13 16:59:51 +0100

    post discussion with advisor, final edits to my DAG. adding a latent node, cas9 activity, in which both promoter and cas9 tag presence converge into to have a combined effect on allele inherited

--- final-DAGfor-submission.R
+++ final-DAGfor-submission.R
@@ -4,31 +4,33 @@ library(tidyverse)
 
 # The final DAG with all confounders included
 # question 4
-
 final_assignment_dag <- dagify(
-  
-  cas9_tagging ~ promoter,
-  allele_inherited ~ promoter, # This is the confounder that affects both the exposure and the outcome
-  allele_inherited ~ cas9_tagging,
+  cas9_activity ~ cas9_tagging, 
+  cas9_activity ~ promoter, 
   allele_inherited ~ generation,
   allele_inherited ~ transgenic_line,
+  allele_inherited ~ cas9_activity, 
   
   exposure = "cas9_tagging",
   outcome = "allele_inherited",
+  latent = "cas9_activity", 
   
   labels = c(
-    cas9_tagging = "Cas9 Protein Tagging",
+    cas9_tagging = "Cas9 Degron Tagg",
     allele_inherited = "Allele Inherited",
     promoter = "Promoter",
     generation = "Generation",
-    transgenic_line = "Transgenic Line"
+    transgenic_line = "Transgenic Line",
+    U = "Cas9 Activity"
   )
 )
 
+# Plotting with ggdag_status will automatically distinguish the latent node
 ggdag_status(final_assignment_dag, use_labels = "label", text = FALSE) +
   theme_dag() +
   theme(legend.position = "bottom") +
   labs(title = "Causal DAG for Cas9 Tagging Inheritance")
+
 
 # checking for colliders
 # question 6
@@ -38,6 +40,7 @@ ggdag_collider(final_assignment_dag) +
   theme(legend.position = "bottom") +
   labs(title = "Collider Check",
        subtitle = "Variables with arrows pointing into them")
+
 
 
 
